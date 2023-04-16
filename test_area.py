6cdc965afbfdeab595c195598dbdfc38e13ae322
angrycompany16 <dpersson16@gmail.com>
2023-04-16 16:01:33 +0200

    test syntax error

--- test_area.py
+++ test_area.py
@@ -7,7 +7,7 @@ def test_calculate_area_square():
   
 def test_calculate_area_square_negative():  
     with pytest.raises(TypeError):  
-        calculate_area_square(-2)  
+        calculate_area_squuare(-2)  
   
 def test_calculate_area_square_string():  
     with pytest.raises(TypeError):  
